4eeb7696573addf1e81ba4e0e0b11b93efeda07f
Matias Brito <matiasbrito32@gmail.com>
2020-08-03 19:04:01 -0300

    Diseño de acceso al juego

--- Maymen.cpp
+++ Maymen.cpp
@@ -1,1 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
+void Esquema();
+
+main(){
+	Esquema();
+	
+	
+	
+	system("PAUSE");
+}
+void Esquema(){
+	int Aux;
+	printf("\nComenzar Juego?  (*1* SI  *0* NO): ");
+	scanf("%d", &Aux);
+	
+	if(Aux>0){
+		printf("\nBienvenido!   **MAYMEN**\n\n");
+	}
+	else{
+		printf("\nSaliste del juego\n\n");
+	}
+
+}
